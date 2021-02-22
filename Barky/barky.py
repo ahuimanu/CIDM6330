cfdd69cd2ba12505e9299be002cc4f48b7ec6312
Jeffry Babb <ahuimanu@gmail.com>
2021-02-22 00:11:45 -0600

    updates to Barky

--- Barky/barky.py
+++ Barky/barky.py
@@ -1,4 +1,4 @@
-'''
+"""
 This module provides the presentation layer and can be consdired "the program."
 
 This module facilitates an infinite loop that:
@@ -24,10 +24,11 @@ This modular design, which separates concerns, provides opportunities for extens
 2. Add a command class that performs the business logic you need in commands.py.
 3. Hook up the new command to a new menu option in barky.py.
 
-'''
+"""
 import os
 
 import commands
+
 
 class Option:
     def __init__(self, name, command, prep_call=None):
@@ -44,14 +45,19 @@ class Option:
         return self.name
 
 
+def clear_screen():
+    clear = "cls" if os.name == "nt" else "clear"
+    os.system(clear)
+
+
 def print_options(options):
-    '''
+    """
     1. Print the keyboard key for the user to enter to choose the option.
     2. Print the option text.
-    3. Check if the user’s input matches an option and, if so, choose it.    
-    '''
+    3. Check if the user’s input matches an option and, if so, choose it.
+    """
     for shortcut, option in options.items():
-        print(f'({shortcut}) {option}')
+        print(f"({shortcut}) {option}")
     print()
 
 
@@ -60,64 +66,101 @@ def option_choice_is_valid(choice, options):
 
 
 def get_option_choice(options):
-    '''
+    """
     1. Prompt the user to enter a choice, using Python’s built-in input function.
     2. If the user’s choice matches one of those listed, call that option’s choose method.
-    3. Otherwise, repeat.    
-    '''
-    choice = input('Choose an option: ')
+    3. Otherwise, repeat.
+    """
+    choice = input("Choose an option: ")
     while not option_choice_is_valid(choice, options):
-        print('Invalid choice')
-        choice = input('Choose an option: ')
+        print("Invalid choice")
+        choice = input("Choose an option: ")
     return options[choice.upper()]
 
 
 def get_user_input(label, required=True):
-    value = input(f'{label}: ') or None
+    value = input(f"{label}: ") or None
     while required and not value:
-        value = input(f'{label}: ') or None
+        value = input(f"{label}: ") or None
     return value
 
 
 def get_new_bookmark_data():
     return {
-        'title': get_user_input('Title'),
-        'url': get_user_input('URL'),
-        'notes': get_user_input('Notes', required=False),
+        "title": get_user_input("Title"),
+        "url": get_user_input("URL"),
+        "notes": get_user_input("Notes", required=False),
     }
 
 
 def get_bookmark_id_for_deletion():
-    return get_user_input('Enter a bookmark ID to delete')
+    return get_user_input("Enter a bookmark ID to delete")
 
 
-def clear_screen():
-    clear = 'cls' if os.name == 'nt' else 'clear'
-    os.system(clear)
+def get_github_import_options():
+    return {
+        "github_username": get_user_input("GitHub username"),
+        "preserve_timestamps": get_user_input(
+            "Preserve timestamps [Y/n]", required=False
+        )
+        in {"Y", "y", None},
+    }
+
+
+def get_new_bookmark_info():
+    bookmark_id = get_user_input("Enter a bookmark ID to edit")
+    field = get_user_input("Choose a value to edit (title, URL, notes)")
+    new_value = get_user_input(f"Enter the new value for {field}")
+    return {
+        "id": bookmark_id,
+        "update": {field: new_value},
+    }
+
 
 def loop():
+
+    clear_screen()
     # All steps for showing and selecting options
     # https://www.w3schools.com/python/python_dictionaries.asp
     options = {
-        'A': Option('Add a bookmark', commands.AddBookmarkCommand(), prep_call=get_new_bookmark_data),
-        'B': Option('List bookmarks by date', commands.ListBookmarksCommand()),
-        'T': Option('List bookmarks by title', commands.ListBookmarksCommand(order_by='title')),
-        'D': Option('Delete a bookmark', commands.DeleteBookmarkCommand(), prep_call=get_bookmark_id_for_deletion),
-        'Q': Option('Quit', commands.QuitCommand()),
+        "A": Option(
+            "Add a bookmark",
+            commands.AddBookmarkCommand(),
+            prep_call=get_new_bookmark_data,
+        ),
+        "B": Option("List bookmarks by date", commands.ListBookmarksCommand()),
+        "T": Option(
+            "List bookmarks by title", commands.ListBookmarksCommand(order_by="title")
+        ),
+        "E": Option(
+            "Edit a bookmark",
+            commands.EditBookmarkCommand(),
+            prep_call=get_new_bookmark_info,
+        ),
+        "D": Option(
+            "Delete a bookmark",
+            commands.DeleteBookmarkCommand(),
+            prep_call=get_bookmark_id_for_deletion,
+        ),
+        "G": Option(
+            "Import GitHub stars",
+            commands.ImportGitHubStarsCommand(),
+            prep_call=get_github_import_options,
+        ),
+        "Q": Option("Quit", commands.QuitCommand()),
     }
-    clear_screen()
-    print_options(options)    
+    print_options(options)
+
     chosen_option = get_option_choice(options)
     clear_screen()
     chosen_option.choose()
-    _ = input('Press ENTER to return to menu')
+    _ = input("Press ENTER to return to menu")
 
 
-# this ensures that this module runs first 
-if __name__ == '__main__':
+# this ensures that this module runs first
+if __name__ == "__main__":
     commands.CreateBookmarksTableCommand().execute()
 
     # endless program loop
     while True:
         loop()
-
