c34c0def7221e02c48733bdd7b49858b64c76a51
Jeffry Babb <ahuimanu@gmail.com>
2021-03-06 18:01:25 -0600

    simple flask demonstration

--- FlaskSA/hello.py
+++ FlaskSA/hello.py
@@ -1,8 +1,34 @@
+# python imports
+# third-party imports
+# your own imports
+
 from flask import Flask
+from markupsafe import escape
+
 from dotenv import load_dotenv
+
 load_dotenv()
+
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+@app.route("/login")
+def login():
+    return "login"
+
+
+@app.route("/user/<username>")
+def profile(username):
+    return f"{escape(username)}'s profile"
+    # return '{}\'s profile'.format(escape(username))
+
+
+@app.route("/me")
+def me_api():
+    return {"username": "jeff", "theme": "dark", "image": "me.jpg"}
