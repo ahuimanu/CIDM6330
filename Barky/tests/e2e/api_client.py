9153d230de244ecb5d54a3f4088b6b0afa0ce1cb
Jeffry Babb <ahuimanu@gmail.com>
2021-04-11 17:26:20 -0500

    created an integration test

--- Barky/tests/e2e/api_client.py
+++ Barky/tests/e2e/api_client.py
@@ -1,4 +1,3 @@
-
 import requests
 from barkylib import config
 
--- Barky/tests/e2e/test_api.py
+++ Barky/tests/e2e/test_api.py
@@ -2,11 +2,13 @@ import pytest
 
 import requests
 
-LOCALHOST = 'http://127.0.0.1:5000/'
+LOCALHOST = "http://127.0.0.1:5000/"
+
 
 def test_api_can_connect():
     res = requests.get(LOCALHOST)
     assert res != None
+
 
 def test_api_index():
     res = requests.get(LOCALHOST)
