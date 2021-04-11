f355a41f6ab615a3801f12cff26abdc92c889ac3
Jeffry Babb <ahuimanu@gmail.com>
2021-04-11 14:18:27 -0500

    convert Bookmark to a dataclass

--- Barky/src/barkylib/domain/models.py
+++ Barky/src/barkylib/domain/models.py
@@ -1,38 +1,25 @@
-from sqlalchemy import Column, String, Integer, Date
-from sqlalchemy.ext.declarative import declarative_base
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import List, Optional
 
-Base = declarative_base()
+
+"""
+Pure domain bookmark:
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT NOT NULL,
+url TEXT NOT NULL,
+notes TEXT,
+date_added TEXT NOT NULL
+date_edited TEXT NOT NULL
+"""
 
 
+@dataclass
 class Bookmark:
-    """
-    Pure domain bookmark:
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    url TEXT NOT NULL,
-    notes TEXT,
-    date_added TEXT NOT NULL
-    date_edited TEXT NOT NULL
-    """
-
-    def __init__(self, id, title, url, notes, date_added) -> None:
-        self.id = id
-        self.title = title
-        self.url = url
-        self.notes = notes
-        self.date_added = date_added
-        self.date_edited = self.date_added
-
-
-# class BookmarkModel(Base):
-#     """
-#     Declarative SQLA model
-#     """
-
-#     __tablename__ = "bookmarks"
-
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String(255))
-#     url = Column(String(255))
-#     notes = Column(String(255))
-#     date_added = Column(Date)
+    id: int
+    title: str
+    url: str
+    notes: str
+    date_added: datetime
+    date_edited: datetime
