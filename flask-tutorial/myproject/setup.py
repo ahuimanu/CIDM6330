a497e593b69368823b5b9d775ffbce935665cbce
Jeffry Babb <ahuimanu@gmail.com>
2022-01-25 10:37:56 -0600

    updated for tests

---
+++ flask-tutorial/myproject/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
+
