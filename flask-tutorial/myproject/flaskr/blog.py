1f5ff2cc8d52c7a15fa92712d8e67fba35a19360
Jeffry Babb <ahuimanu@gmail.com>
2022-02-09 20:26:37 -0600

    updated flask

--- flask-tutorial/myproject/flaskr/blog.py
+++ flask-tutorial/myproject/flaskr/blog.py
@@ -17,7 +17,8 @@ def index():
         "SELECT p.id, title, body, created, author_id, username"
         " FROM post p JOIN user u ON p.author_id = u.id"
         " ORDER BY created DESC"
-    )
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
 
 
 @bpblog.route("/create", methods=("GET", "POST"))
