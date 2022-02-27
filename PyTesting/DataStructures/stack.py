4898b32c39c409e61bf5ef6c69c57e914ce96a28
Jeffry Babb <ahuimanu@gmail.com>
2022-02-27 14:49:50 -0600

    progress on Pytesting

--- PyTesting/DataStructures/stack.py
+++ PyTesting/DataStructures/stack.py
@@ -1,2 +1,17 @@
 class Stack:
-    pass
+    def __init__(self):
+        self._storage = []
+
+    def __len__(self) -> int:
+        return len(self._storage)
+
+    def push(self, item):
+        self._storage.append(item)
+
+    def pop(self):
+        try:
+            item = self._storage.pop()
+        except IndexError:
+            item = None
+        
+        return item
--- PyTesting/Tests/test_stack.py
+++ PyTesting/Tests/test_stack.py
@@ -1,12 +1,29 @@
+import pytest
 from DataStructures.stack import Stack
 
+# pytest fixtures help with the arrange portion of testing
 
-def test_can_create_stack():
+@pytest.fixture
+def t_stack():
+    return Stack()
+
+# we are injecting the fixture here
+def test_can_create_stack(t_stack):
     # arrange
     # act
-    s = Stack()
 
     # https://docs.python.org/3.8/library/functions.html#isinstance
     # assert
-    assert isinstance(s, Stack)
-    assert len(s) == 0
+    assert isinstance(t_stack, Stack)
+    assert len(t_stack) == 0
+
+# we are injecting the fixture here
+def test_can_push_to_stack(t_stack):
+    t_stack.push(3)
+    assert len(t_stack) == 1
+
+def test_can_pop_from_stack(t_stack):
+    pushed_item = "test item"
+    t_stack.push("test item")
+    assert t_stack.pop() == pushed_item
+    assert t_stack.pop() is None
