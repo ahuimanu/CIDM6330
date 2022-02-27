a0d683a30e46cde50a4c8f2b50bc6c3c0e455584
Jeffry Babb <ahuimanu@gmail.com>
2022-02-27 15:38:49 -0600

    changes from video walkthrough

--- PyTesting/DataStructures/stack.py
+++ PyTesting/DataStructures/stack.py
@@ -2,7 +2,7 @@ class Stack:
     def __init__(self):
         self._storage = []
 
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self._storage)
 
     def push(self, item):
--- PyTesting/Tests/test_stack.py
+++ PyTesting/Tests/test_stack.py
@@ -1,29 +1,24 @@
 import pytest
 from DataStructures.stack import Stack
 
-# pytest fixtures help with the arrange portion of testing
-
 @pytest.fixture
 def t_stack():
     return Stack()
 
-# we are injecting the fixture here
 def test_can_create_stack(t_stack):
     # arrange
     # act
-
     # https://docs.python.org/3.8/library/functions.html#isinstance
     # assert
     assert isinstance(t_stack, Stack)
     assert len(t_stack) == 0
 
-# we are injecting the fixture here
 def test_can_push_to_stack(t_stack):
-    t_stack.push(3)
+    t_stack.push(99)
     assert len(t_stack) == 1
 
 def test_can_pop_from_stack(t_stack):
     pushed_item = "test item"
-    t_stack.push("test item")
+    t_stack.push(pushed_item)
     assert t_stack.pop() == pushed_item
     assert t_stack.pop() is None
