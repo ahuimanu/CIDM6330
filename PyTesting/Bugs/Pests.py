5e64317c99ccc6e08630b4d3a3eaed8c9b5003ae
Jeffry Babb <ahuimanu@gmail.com>
2022-03-09 23:37:21 -0600

    quick testing example

---
+++ PyTesting/Bugs/Pests.py
@@ -0,0 +1,22 @@
+class Pest:
+    def __init__(self):    
+        self.species = None
+
+class PestObservation:
+    def __init__(self):
+        self.city = None
+        self.county = None
+        self.obs_date = None
+
+class MaladyType:
+    def __init__(self):
+        self.known_vectors : Pest = []
+
+class ImpactObservation:
+    def __init__(self):
+        self.malady_type = None
+        self.species = None
+        self.city = None
+        self.county = None
+        self.obs_date = None
+        
