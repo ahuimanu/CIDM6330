2d3a738ca89c3b77b014694e8d9e19d279639d8e
Jeffry Babb <ahuimanu@gmail.com>
2022-03-10 21:28:43 -0600

    updated tests for aviation formulary

--- acme/avform/conversions.py
+++ acme/avform/conversions.py
@@ -21,3 +21,21 @@ def knots_to_fps(knots: float) -> float:
 def knots_to_mph(knots: float) -> float:
     return knots * 1.150779
 
+def mph_to_knots(mph: float) -> float:
+    return mph * 0.868976
+
+def mph_to_kph(mph: float) -> float:
+    return mph * 1.609344
+
+def mph_to_fps(mph: float) -> float:
+    return mph * 1.466667
+
+def kph_to_knots(kph: float) -> float:
+    return kph * 0.539968
+
+def kph_to_fps(kph: float) -> float:
+    return kph * 0.911344
+
+def kph_to_mph(kph: float) -> float:
+    return kph * 0.621371
+
--- acme/tests/test_avform.py
+++ acme/tests/test_avform.py
@@ -3,6 +3,9 @@ from avform.conversions import (
     knots_to_kph,
     knots_to_fps,
     knots_to_mph,
+    mph_to_knots,
+    mph_to_kph,
+    mph_to_fps,
 )
 
 def test_knots_to_kph():
@@ -19,3 +22,29 @@ def test_knots_to_mph():
     knots = 3
     mph = knots_to_mph(knots)
     assert mph == 3.452337
+
+def test_mph_to_knots():
+    mph = 3
+    knots = mph_to_knots(mph)
+    assert knots == 2.606928
+
+def test_mph_to_kph():
+    mph = 3
+    kph = mph_to_kph(mph)
+    assert kph == 4.828032
+
+def test_mph_to_fps():
+    mph = 3
+    fps = mph_to_fps(3)
+    assert fps == 4.400001
+
+
+
+# def kph_to_knots(kph: float) -> float:
+#     return kph * 0.539968
+
+# def kph_to_fps(kph: float) -> float:
+#     return kph * 0.911344
+
+# def kph_to_mph(kph: float) -> float:
+#     return kph * 0.621371    
