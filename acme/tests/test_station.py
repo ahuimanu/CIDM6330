6c8e414e520d5ed1f885483b769c324b5d0612eb
Jeffry Babb <ahuimanu@gmail.com>
2022-03-19 17:36:35 -0500

    updated for testing

--- acme/tests/test_station.py
+++ acme/tests/test_station.py
@@ -2,11 +2,52 @@ from weather.station import NOAAADDSStationHelper as station_helper
 from weather.station import Station
 
 
+def test_can_get_xml_from_station_id():
+    xml = station_helper._request_noaa_xml("KAMA")
+    assert len(xml) > 0
+
+
+def test_can_parse_returned_noaa_xml():
+    xml_to_parse = """ \
+<response xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.0" xsi:noNamespaceSchemaLocation="http://www.aviationweather.gov/static/adds/schema/station1_0.xsd">
+<request_index>286475603</request_index>
+<data_source name="stations"/>
+<request type="retrieve"/>
+<errors/>
+<warnings/>
+<time_taken_ms>3</time_taken_ms>
+<data num_results="1">
+<Station>
+<station_id>KDEN</station_id>
+<wmo_id>72565</wmo_id>
+<latitude>39.85</latitude>
+<longitude>-104.65</longitude>
+<elevation_m>1656.0</elevation_m>
+<site>DENVER (DIA)</site>
+<state>CO</state>
+<country>US</country>
+<site_type>
+<METAR/>
+<TAF/>
+</site_type>
+</Station>
+</data>
+</response>
+    """
+    # arrange
+    # act
+    tree_root = station_helper._parse_noaa_xml(xml_to_parse)
+
+    print(f"\nin parse parse_noaa_xml root tag is: {tree_root.tag}")
+
+    assert tree_root.tag == "response"
+
+
 def test_can_get_station_from_station_id():
 
     # arrange
     # act
-    station = station_helper.get_station_from_station_id("XXXX")
+    station = station_helper.get_station_from_station_id("KAMA")
 
     # assert
     assert isinstance(station, Station)
--- acme/weather/station.py
+++ acme/weather/station.py
@@ -1,7 +1,14 @@
-import xml.etree.ElementTree as ET
+import string
 from abc import ABC, abstractmethod, abstractproperty
 from dataclasses import dataclass
 from enum import Enum, IntEnum
+from turtle import st
+from typing import TypeAlias
+from urllib.error import HTTPError
+from xml.etree.ElementTree import Element, fromstring
+
+import requests
+from iniconfig import ParseError
 
 
 class StationType(IntEnum):
@@ -26,7 +33,6 @@ class StationType(IntEnum):
 class Station:
     """
     https://aviationweather.gov/dataserver/fields?datatype=station
-
     """
 
     station_id: str = ""  # The 4-letter station specifier
@@ -65,12 +71,78 @@ class StationHelper(ABC):
 
 
 class NOAAADDSStationHelper(StationHelper):
+
+    NOAA_ADDS_URL = (
+        f"https://aviationweather-bldr.ncep.noaa.gov/adds/dataserver_current/httpparam"
+    )
+    # guide url params: dataSource=stations&requestType=retrieve&format=xml&stationString=KAMA
+    NOAA_ADDS_FORMAT = "xml"
+
     @staticmethod
     def get_station_from_station_id(station_id: str) -> Station:
-        return Station()
 
-    def _parse_noaa_xml(xml: str) -> Station:
-        pass
+        xml = NOAAADDSStationHelper._request_noaa_xml(station_id)
+        tree_root = NOAAADDSStationHelper._parse_noaa_xml(xml)
+        station = NOAAADDSStationHelper._create_station_from_element(tree_root)
+
+        return station
+
+    @staticmethod
+    def _request_noaa_xml(station_id: str) -> str:
+
+        # prepare url
+        url = NOAAADDSStationHelper.NOAA_ADDS_URL
+        format = NOAAADDSStationHelper.NOAA_ADDS_FORMAT
+
+        stations_params = {
+            "dataSource": "stations",
+            "requestType": "retrieve",
+            "format": format,
+            "stationString": station_id.strip(),
+        }
+
+        # start an empty return value
+        response_xml = ""
+
+        try:
+            noaa_response = requests.get(url, stations_params)
+        except ConnectionError:
+            response_xml = ""
+        except HTTPError:
+            response_xml = ""
+
+        if noaa_response.status_code == 200:
+            response_xml = noaa_response.text
+
+        return response_xml
+
+    def _parse_noaa_xml(xml: str) -> Element:
+
+        # prepare - create station object
+        # station_id: str = ""  # The 4-letter station specifier
+        # wmo_id: str = ""  # Four-letter WMO Id for the station
+        # latitude: float = 0.0  # The latitude in decimal degrees
+        # longitude: float = 0.0  # The longitude in decimal degrees
+        # elevation_m: float = (
+        #     0.0  # The elevation of the station in MSL (above mean sea-level)
+        # )
+        # site: str = ""  # The "common" name/human-readable name of the station
+        # state: str = (
+        #     ""  # The two-letter abbreviation for the U.S. state or Canadian province
+        # )
+        # country: str = ""  # The two-letter country abbreviation
+        # site_type: StationType = StationType.METAR
+
+        try:
+            xml_tree_root = fromstring(xml)
+        except ParseError:
+            xml_tree_root = None
+
+        return xml_tree_root
+
+    @staticmethod
+    def _create_station_from_element(xml_tree_root: Element):
+        return Station()
 
     @staticmethod
     def get_station_from_lat_lon(latitude: float, longitude: float) -> Station:
