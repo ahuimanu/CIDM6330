b071fa8b3185c642f0777eda6b09a267846436d1
Jeffry Babb <ahuimanu@gmail.com>
2023-03-27 18:20:41 -0500

    updated chapter 4

---
+++ projects/APP2023/code-chapter_04_service_layer/conftest.py
@@ -0,0 +1,138 @@
+# pylint: disable=redefined-outer-name
+import time
+from pathlib import Path
+
+import pytest
+import requests
+from requests.exceptions import ConnectionError
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+from orm import mapper_registry, start_mappers
+import config
+from flask_app import create_app
+
+# chapter reworked to follow
+# https://flask.palletsprojects.com/en/2.2.x/patterns/appfactories/#application-factories
+# and
+# https://flask.palletsprojects.com/en/2.2.x/testing/#testing-flask-applications
+# P&G might be too fixated on Docker containers with their example.
+
+
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine(f"sqlite:///:memory:")
+    mapper_registry.metadata.create_all(engine)
+    return engine
+
+@pytest.fixture
+def file_sqlite_db():
+    engine = create_engine(f"sqlite:///bookmarks.db")
+    mapper_registry.metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def session(file_sqlite_db):
+    start_mappers()
+    yield sessionmaker(bind=file_sqlite_db)()
+    clear_mappers()
+
+
+@pytest.fixture
+def flask_api(session):
+    app = create_app()
+    app.config.update({"TESTING": True})
+    return app
+
+
+@pytest.fixture
+def test_client(flask_api):
+    return flask_api.test_client()
+
+
+# def wait_for_postgres_to_come_up(engine):
+#     deadline = time.time() + 10
+#     while time.time() < deadline:
+#         try:
+#             return engine.connect()
+#         except OperationalError:
+#             time.sleep(0.5)
+#     pytest.fail("Postgres never came up")
+
+
+# def wait_for_webapp_to_come_up():
+#     deadline = time.time() + 10
+#     url = config.get_api_url()
+#     while time.time() < deadline:
+#         try:
+#             return requests.get(url)
+#         except ConnectionError:
+#             time.sleep(0.5)
+#     pytest.fail("API never came up")
+
+
+# @pytest.fixture(scope="session")
+# def postgres_db():
+#     engine = create_engine(config.get_postgres_uri())
+#     wait_for_postgres_to_come_up(engine)
+#     mapper_registry.metadata.create_all(engine)
+#     return engine
+
+
+# @pytest.fixture
+# def postgres_session(postgres_db):
+#     start_mappers()
+#     yield sessionmaker(bind=postgres_db)()
+#     clear_mappers()
+
+
+@pytest.fixture
+def add_stock(session):
+    # take care and note that this fixture takes care of adding in records to the database.
+    batches_added = set()
+    skus_added = set()
+
+    def _add_stock(lines):
+        for ref, sku, qty, eta in lines:
+            session.execute(
+                text(
+                    "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+                    " VALUES (:ref, :sku, :qty, :eta)"
+                ),
+                dict(ref=ref, sku=sku, qty=qty, eta=eta),
+            )
+            [[batch_id]] = session.execute(
+                text("SELECT id FROM batches WHERE reference=:ref AND sku=:sku"),
+                dict(ref=ref, sku=sku),
+            )
+            batches_added.add(batch_id)
+            skus_added.add(sku)
+        session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        session.execute(
+            text("DELETE FROM allocations WHERE batch_id=:batch_id"),
+            dict(batch_id=batch_id),
+        )
+        session.execute(
+            text("DELETE FROM batches WHERE id=:batch_id"),
+            dict(batch_id=batch_id),
+        )
+    for sku in skus_added:
+        session.execute(
+            text("DELETE FROM order_lines WHERE sku=:sku"),
+            dict(sku=sku),
+        )
+        
+    session.commit()
+
+
+# @pytest.fixture
+# def restart_api():
+#     (Path(__file__).parent / "flask_app.py").touch()
+#     time.sleep(0.5)
---
+++ projects/APP2023/code-chapter_04_service_layer/orm.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import registry, relationship
+
+import model
+
+# https://docs.sqlalchemy.org/en/20/orm/mapping_styles.html#imperative-mapping
+# using SQLAlchemy 2.0-style imperative mapping
+mapper_registry = registry()
+
+order_lines = Table(
+    "order_lines",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("sku", String(255)),
+    Column("qty", Integer, nullable=False),
+    Column("orderid", String(255)),
+)
+
+batches = Table(
+    "batches",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
+
+def start_mappers():
+    lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper,
+                secondary=allocations,
+                collection_class=set,
+            )
+        },
+    )
