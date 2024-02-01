e238cc7a443b261767ca2c97cc38267d85cb36ac
Jeffry Babb <ahuimanu@gmail.com>
2024-02-01 17:01:14 -0600

    updated tests

---
+++ Testing/About/tests/test.py
@@ -0,0 +1,14 @@
+import unittest
+from sum_thing import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2]
+        result = sum(data)
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
