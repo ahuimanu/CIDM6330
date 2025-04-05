66de584cf4283ed9e463627b970ad8e20fb3006f
Jeffry Babb <ahuimanu@gmail.com>
2025-04-05 14:38:36 -0500

    updated data files

--- CIDM6330-Spring2025/Evolution_05_TDD_BDD_DDD/2025/travelmanagement/apilist/tasks.py
+++ CIDM6330-Spring2025/Evolution_05_TDD_BDD_DDD/2025/travelmanagement/apilist/tasks.py
@@ -2,20 +2,25 @@ from django.core.cache import cache
 from celery import shared_task
 import requests
 
+"""
+This module contains the Celery task to fetch and store temperature data for cities.
+Credit to: Afroza Nowshin
+"""
 
 @shared_task
 def fetch_and_store_temperature():
     try:
-        response = requests.get('https://raw.githubusercontent.com/strativ-dev/technical-screening-test/main/bd-districts.json')
+        # https://raw.githubusercontent.com/ahuimanu/CIDM6330/SPRING2025/Resources/cities.json
+        response = requests.get('https://raw.githubusercontent.com/ahuimanu/CIDM6330/SPRING2025/Resources/cities.json')
         response.raise_for_status()
 
-        districts_data = response.json().get('districts', [])
+        cities_data = response.json().get('cities', [])
 
         all_temperatures = []
 
-        for district in districts_data:
-            latitude = district.get('lat')
-            longitude = district.get('long')
+        for city in cities_data:
+            latitude = city.get('lat')
+            longitude = city.get('long')
 
             if latitude is not None and longitude is not None:
                 api_url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&timezone=GMT&forecast_days=7'
@@ -28,7 +33,7 @@ def fetch_and_store_temperature():
                 temperature_at_2pm = hourly_data.get('temperature_2m', [])
 
                 # Cache key for each district without specifying a travel date
-                cache_key = f'temperature_at_2pm_{district["name"]}'
+                cache_key = f'temperature_at_2pm_{city["name"]}'
                 cache.set(cache_key, temperature_at_2pm)
 
                 # print("checking", cache.get(cache_key))
