a256c7e628e7165826f54a91e46f2b1ca1fe7ecd
Jeffry Babb <ahuimanu@gmail.com>
2025-04-05 15:02:39 -0500

    updated for celery EVO 5

--- CIDM6330-Spring2025/Evolution_05_TDD_BDD_DDD/2025/travelmanagement/travelmanagement/settings.py
+++ CIDM6330-Spring2025/Evolution_05_TDD_BDD_DDD/2025/travelmanagement/travelmanagement/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_celery_results",
+    "django_celery_beat",
     "apilist.apps.ApilistConfig",
 ]
 
@@ -133,6 +135,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_IMPORTS = ('apilist.tasks',) 
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 CELERY_TIMEZONE = os.getenv('CELERY_TIMEZONE', 'UTC')
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
